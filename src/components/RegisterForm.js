aa1a817b97b593ec99b9e3769b9bd8bf1201ff15
stephanholly <steph.c.holly@gmail.com>
2019-02-27 17:55:24 -0700

    register form

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 
 class RegisterForm extends Component {
+
+  alert=()=>{
+    alert("thank you for subscribing! We will reach out to you with more information soon!")
+  }
+
   render() {
     return (
       <div className="container">
@@ -10,15 +15,15 @@ class RegisterForm extends Component {
           <form action="https://swimmerstrength.us20.list-manage.com/subscribe/post?u=4b4c58543e446a8afa2e17ebb&amp;id=338ee2ec3a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" >
             <div className="inner-form">
               <div className="left-inputs">
-                <input className="text-input-contact-first" type="text" placeholder="Name"  name="NAME" id="mce-NAME"/>
-                <input className="text-input-contact" type="email" placeholder="Email address"  name="EMAIL" id="mce-EMAIL"/>
-                <input className="text-input-contact" type="text" placeholder="Subject" name="SUBJECT" id="mce-SUBJECT"/>
+                <input className="text-input-contact-first required" type="text" placeholder="Name"  name="NAME" id="mce-NAME"/>
+                <input className="text-input-contact required" type="email" placeholder="Email address"  name="EMAIL" id="mce-EMAIL"/>
+                <input className="text-input-contact required" type="text" placeholder="Subject" name="SUBJECT" id="mce-SUBJECT"/>
               </div>
               <div className="right-inputs">
-                <textarea name="message" className="text-input-textarea" type="text" placeholder="Message" name="MESSAGE" id="mce-MESSAGE"></textarea>
+                <textarea name="message" className="text-input-textarea required" type="text" placeholder="Message" name="MESSAGE" id="mce-MESSAGE"></textarea>
               </div>
             </div>
-            <input className="text-input-submit" type="submit" value="Subscribe" name="subscribe" />
+            <input className="text-input-submit" type="submit" value="Subscribe" name="subscribe" onClick={()=> this.alert()}/>
           </form>
         </div>
       </div>
